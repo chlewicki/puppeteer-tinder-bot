865a7c42bd45c941ceec23bd5eab970eb893e15a
Marcin Chlewicki <chlewicki@gmail.com>
2020-02-16 17:24:21 +0100

    cvbnm

---
+++ index.js
@@ -0,0 +1,50 @@
+const puppeteer = require('puppeteer');
+require('dotenv').config();
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+
+    const page = await browser.newPage();
+    await page.goto('https://tinder.com/', {
+        waitUntil: 'networkidle2'
+    });
+    const login = async () => {
+        try {
+            const pageTarget = page.target();
+            const [buttonFacebook] = await page.$x('//*[@id="modal-manager"]/div/div/div/div/div[3]/div[2]/button')
+            buttonFacebook.click()
+
+            const newTarget = await browser.waitForTarget(target => target.opener() === pageTarget);
+            const newPage = await newTarget.page();
+            await newPage.waitForSelector("body");
+            await newPage.type('input[name="email"]', process.env.USER_NAME)
+            await newPage.type('input[name="pass"]', process.env.PASSWORD)
+            const [buttonConfirm] = await newPage.$x('//*[@id="u_0_0"]')
+            buttonConfirm.click()
+        } catch (err) {
+            console.error(err)
+        }
+    }
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve()
+        }, 3000)
+    })
+    await login()
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve()
+        }, 5000)
+    })
+    console.log('login finish')
+    const [buttonAllow1] = await page.$x('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+    buttonAllow1.click()
+
+    const [buttonAllow2] = await page.$x('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+    buttonAllow2.click()
+
+    // await browser.close();
+
+})();
