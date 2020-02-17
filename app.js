50efabf0e804fc6ca3a41692d8d182522aebf988
Marcin Chlewicki <chlewicki@gmail.com>
2020-02-17 16:51:04 +0100

    refactor to class

--- app.js
+++ app.js
@@ -1,14 +1,89 @@
 const puppeteer = require('puppeteer');
 require('dotenv').config();
 
-const sleep = async (mil) => {
-    return new Promise(resolve => setTimeout(resolve, mil));
+class Tinder {
+    constructor(page, browser) {
+        this.page = page;
+        this.browser = browser;
+    }
+
+    async sleep(mil) {
+        return new Promise(resolve => setTimeout(resolve, mil));
+    }
+
+    async login() {
+        try {
+            await this.sleep(3000);
+            const pageTarget = this.page.target();
+            const [buttonFacebook] = await this.page.$x('//*[@id="modal-manager"]/div/div/div/div/div[3]/div[2]/button')
+            buttonFacebook.click()
+
+            const newTarget = await this.browser.waitForTarget(target => target.opener() === pageTarget);
+            const newPage = await newTarget.page();
+            await newPage.waitForSelector("body");
+            await newPage.type('input[name="email"]', process.env.USER_NAME)
+            await newPage.type('input[name="pass"]', process.env.PASSWORD)
+            const [buttonConfirm] = await newPage.$x('//*[@id="u_0_0"]')
+            buttonConfirm.click()
+            await this.sleep(5000)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    async like() {
+        try {
+            const [buttonLike] = await this.page.$x('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[3]')
+            buttonLike.click()
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    async closePopup() {
+        try {
+            const [buttonPopup] = await this.page.$x('//*[@id="modal-manager"]/div/div/div[2]/button[2]')
+            buttonPopup.click()
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    async closeMatch() {
+        try {
+            const [buttonMatch] = await this.page.$x('//*[@id="modal-manager-canvas"]/div/div/div[1]/div/div[3]/a')
+            buttonMatch.click()
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    async findDate() {
+        let swipe = true;
+        while (swipe) {
+            await this.sleep(1000);
+            try {
+                await this.like();
+            } catch (err) {
+                try {
+                    await this.closeMatch();
+                } catch (err) {
+                    try {
+                        await this.closePopup();
+                    } catch (err) {
+                        swipe = false
+                        await this.browser.close();
+                    }
+                }
+            }
+        }
+    }
 }
+
 
 (async () => {
     const browser = await puppeteer.launch({
-        headless: false,
-        executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+        headless: false
     });
     const page = await browser.newPage();
 
@@ -22,71 +97,13 @@ const sleep = async (mil) => {
         longitude: 21.0898869,
         accuracy: 100
     });
+
     await page.goto('https://tinder.com', {
         waitUntil: 'networkidle2'
     });
-    const login = async () => {
-        try {
-            const pageTarget = page.target();
-            const [buttonFacebook] = await page.$x('//*[@id="modal-manager"]/div/div/div/div/div[3]/div[2]/button')
-            buttonFacebook.click()
 
-            const newTarget = await browser.waitForTarget(target => target.opener() === pageTarget);
-            const newPage = await newTarget.page();
-            await newPage.waitForSelector("body");
-            await newPage.type('input[name="email"]', process.env.USER_NAME)
-            await newPage.type('input[name="pass"]', process.env.PASSWORD)
-            const [buttonConfirm] = await newPage.$x('//*[@id="u_0_0"]')
-            buttonConfirm.click()
-        } catch (err) {
-            console.error(err)
-        }
-    }
-    const like = async () => {
-        try {
-            const [buttonLike] = await page.$x('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[3]')
-            buttonLike.click()
-        } catch (err) {
-            console.error(err)
-        }
-    }
-    const closePopup = async () => {
-        try {
-            const [buttonPopup] = await page.$x('//*[@id="modal-manager"]/div/div/div[2]/button[2]')
-            buttonPopup.click()
-        } catch (err) {
-            console.error(err)
-        }
-    }
-    const closeMatch = async () => {
-        try {
-            const [buttonMatch] = await page.$x('//*[@id="modal-manager-canvas"]/div/div/div[1]/div/div[3]/a')
-            buttonMatch.click()
-        } catch (err) {
-            console.error(err)
-        }
-    }
-    await sleep(3000)
-    await login()
+    const bot = new Tinder(page, browser);
 
-    await sleep(5000)
-
-    let swipe = true;
-    while (swipe) {
-        await sleep(1000);
-        try {
-            await like();
-        } catch (err) {
-            try {
-                await closePopup();
-            } catch (err) {
-                try {
-                    await closeMatch();
-                } catch (err) {
-                    swipe = false
-                    await browser.close();
-                }
-            }
-        }
-    }
+    await bot.login()
+    await bot.findDate()
 })();
